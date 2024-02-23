50087305cabd1873f9be74164cbe526f89c10732
aviraj4777 <95161783+aviraj4777@users.noreply.github.com>
2024-02-23 13:44:55 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,17 +1,1 @@
-def is_prime (n):
-    d=n//2
-    if (n<2) : 
-        return False 
-    else: 
-        while (d>1) : 
-            if (n% d==0) : 
-                return False
-            d=d-1
-        return True 
-    
-def main(): 
-    while True: 
-        num = int(input('>')) 
-        print (is_prime(num)) 
-if __name__ == "_main_": 
-    main()
+print("Hello World")
